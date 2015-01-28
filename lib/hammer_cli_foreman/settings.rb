fd8c1e905dc61d8f2354ded07e82a583718046c2
Tomas Strachota <tstrachota@redhat.com>
2015-01-28 16:23:09 +0000

    step - defining output

--- lib/hammer_cli_foreman/settings.rb
+++ lib/hammer_cli_foreman/settings.rb
@@ -7,6 +7,13 @@ module HammerCLIForeman
 
     class ListCommand < HammerCLIForeman::ListCommand
 
+      output do
+        field :name, _('Name')
+        field :value, _('Value')
+        field :description, _('Description')
+      end
+
+      build_options
     end
 
     autoload_subcommands
