c62fcef242d47198763d7a67320170dfa3c00cae
Tomas Strachota <tstrachota@redhat.com>
2015-01-28 14:33:44 +0000

    step - command settings list

---
+++ lib/hammer_cli_foreman/settings.rb
@@ -0,0 +1,14 @@
+
+module HammerCLIForeman
+
+  class Settings < HammerCLIForeman::Command
+
+    resource :settings
+
+    class ListCommand < HammerCLIForeman::ListCommand
+
+    end
+
+  end
+
+end
