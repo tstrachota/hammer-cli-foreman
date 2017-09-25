3dc47a11c9a81967f3a40ccc8af9c87f4895fbd3
Tomas Strachota <tstrachota@redhat.com>
2017-09-25 17:01:57 +0200

    Remove modifying commands

--- lib/hammer_cli_foreman/compute_profile.rb
+++ lib/hammer_cli_foreman/compute_profile.rb
@@ -2,7 +2,6 @@ require 'hammer_cli_foreman/image'
 require 'hammer_cli_foreman/compute_resources/all'
 
 module HammerCLIForeman
-
   class ComputeProfile < HammerCLIForeman::Command
     resource :compute_profiles
 
@@ -24,7 +23,6 @@ module HammerCLIForeman
     end
 
     class ListCommand < HammerCLIForeman::ListCommand
-
       output do
         field :id, _("Id")
         field :name, _("Name")
@@ -76,37 +74,6 @@ module HammerCLIForeman
       build_options
     end
 
-
-    class CreateCommand < HammerCLIForeman::CreateCommand
-      success_message _("Compute resource created")
-      failure_message _("Could not create the compute resource")
-
-      build_options
-
-      validate_options do
-        all(:option_name).required
-      end
-    end
-
-
-    class UpdateCommand < HammerCLIForeman::UpdateCommand
-      success_message _("Compute resource updated")
-      failure_message _("Could not update the compute resource")
-
-      build_options :without => :name
-    end
-
-
-    class DeleteCommand < HammerCLIForeman::DeleteCommand
-      success_message _("Compute resource deleted")
-      failure_message _("Could not delete the compute resource")
-
-      build_options
-    end
-
     autoload_subcommands
   end
-
 end
-
-
